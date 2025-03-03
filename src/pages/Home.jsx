12f67b21eaf0056c46be1608cdb78e0dd024b4e5
ritik wamnkar <wamnkarr@gmail.com>
2025-03-03 18:40:31 +0530

    Updated profile uploader and UI changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaFacebookF } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 function Home() {
+
+    const [image,setImage]=useState(null)
+
+    function handleUploadImage(e){
+       const file=e.target.files[0];
+       if(file){
+        setImage(URL.createObjectURL(file));
+    
+       }
+    }
+
   return (
    <div className='py-1 '>
        <div className='flex flex-col gap-3 mt-4 md:mt-14 lg:gap-8'>
-            <div className='w-[99%]  p-[2px] md:flex gap-6 items-center lg:gap-14 lg:mt-5'>
-                <img className='min-w-[330px] max-h-[330px]  rounded-lg' src="../../public/cvpic.jpg" alt="dffgds" />
+            <div className='w-[99%]  p-[2px] md:flex gap-6 items-center  lg:gap-14 lg:mt-5'>
+                <div className='mb-12 mt-8 w-[310px] h-[310px] bg-white rounded-full overflow-hidden flex justify-center items-center '>
+                    {
+                        image && <img src={image} alt="image not found"  className=' rounded-full object-contain border-2 border-gray-300 mt-[149px]'/>
+                    }
+                    {
+                        image?"":<label htmlFor="fileinput" className='text-black text-xl font-semibold text-gray-500 cursor-pointer '>Upload profile picture <input id='fileinput' type="file" alt='myimage' onChange={handleUploadImage} className='w-full h-full hidden'/></label>
+
+                    }                    
+                    
+                </div>
                 <div className=''>
                     <h1 className='text-2xl font-bold mt-3 sm:text-[30px] md:text-[30px] lg:text-[40px]'>Hi,I'm Vainu Wamnkar</h1>
                     <h2 className='text-xl font-semibold text-blue-600 sm:text-[30px] sm:mt-2 md:text-[30px] md:mt-4 lg:text-[35px]'>Frontend Developer</h2>
